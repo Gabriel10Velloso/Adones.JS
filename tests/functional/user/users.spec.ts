6a52859f85c25dfcec37d172394159a69f2f4f3d
Gabriel <gavelloso@gmail.com>
2022-10-08 13:59:37 -0300

    Migration intall

--- tests/functional/user/users.spec.ts
+++ tests/functional/user/users.spec.ts
@@ -3,10 +3,33 @@ import supertest from 'supertest'
 
 const BASE_URL = `http://${process.env.HOST}:${process.env.PORT}`
 
+/*
+  Retorno do usuário
+  {
+    "users":{
+       id: number,
+       username: string,
+       email: string,
+       password: ' string,
+       avatar: ' string,
+      }
+  }
+*/
 test.group('User', () => {
-  test('Teste User', async () => {
-    const userPayload = { email: 'teste', username: 'teste', password: 'teste' }
-    await supertest(BASE_URL).post('/users').send(userPayload).expect(201)
+  test('Teste User', async ({ assert }) => {
+    const userPayload = {
+      email: 'test@test.com',
+      username: 'test',
+      password: 'test',
+      avatar: 'https://images.com/image/1',
+    }
+    const { body } = await supertest(BASE_URL).post('/users').send(userPayload).expect(201)
+    assert.exists(body.user, 'User undefined')
+    assert.exists(body.user.id, 'Id undefined')
+    assert.equal(body.user.email, userPayload.email)
+    assert.equal(body.user.username, userPayload.username)
+    assert.equal(body.user.avatar, userPayload.avatar)
+    assert.equal(body.user.password, userPayload.password)
   }).pin() // executa apenas um teste
 })
 
